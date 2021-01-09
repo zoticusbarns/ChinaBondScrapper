0afb2f194944fc719cf68615930312f17d771e8e
zoticus-barns <zoticusbarns@gmail.com>
2021-01-09 17:06:23 +0800

    Added requirements.txt and clean up code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ChinaBondScrapper
+
+## Install
+```sh
+git clone https://github.com/zoticusbarns/ChinaBondScrapper.git
+python -m pip install -r requirements.txt
+```
+
+Open main.py using any text editor and modify the few line of code
+```python
+if __name__ == '__main__':
+    date = "2020-12-31"  # yyyy-mm-dd
+    driver_path = ""  # path to your Chrome web driver
+    main(date, driver_path)
+```
+
+Run the script using python in terminal
+
+```sh
+python main.py
+```
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -8,8 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 import logging
 from PIL import Image
 import io
+import argparse
 
 logger = logging.getLogger()
+
 
 def show_element_screen(element):
     imageStream = io.BytesIO(element.screenshot_as_png)
@@ -18,8 +20,9 @@ def show_element_screen(element):
 
 
 def save_element_screen(element, newfilename):
-    imageStream = io.BytesIO(element.screenshot_as_png)
-    img = Image.open(imageStream)
+    img_bytes = element.screenshot_as_png
+    image_stream = io.BytesIO(img_bytes)
+    img = Image.open(image_stream)
     img.save(newfilename)
 
 
@@ -33,14 +36,23 @@ def set_datepicker_date(driver, date: str):
 
     # Set Month and Year
     dropdowns[1].click()
+    dropdowns[1].send_keys(Keys.BACKSPACE)
+    dropdowns[1].send_keys(Keys.BACKSPACE)
     dropdowns[1].send_keys(date[0])
     dropdowns[0].click()
     dropdowns[0].click()
+    dropdowns[0].send_keys(Keys.BACKSPACE)
+    dropdowns[0].send_keys(Keys.BACKSPACE)
+    dropdowns[0].send_keys(Keys.BACKSPACE)
+    dropdowns[0].send_keys(Keys.BACKSPACE)
     dropdowns[0].send_keys(date[1])
     dropdowns[1].click()
 
-    date_element = driver.find_element_by_xpath(f"//table[@class='WdayTable']//td[text()='{date[2]}']")
-    date_element.click()
+    date_element = driver.find_elements_by_xpath(f"//table[@class='WdayTable']//td[text()='{date[2]}']")
+    if int(date[2]) > 15:
+        date_element[-1].click()
+    else:
+        date_element[0].click()
 
     driver.switch_to.default_content()
 
@@ -60,6 +72,8 @@ def get_single_data(table, results, screen):
 
 def collect_data_on_graph(webdriver):
     screen = webdriver.find_element_by_xpath("//div[@id='main']")
+    actions = ActionChains(webdriver)
+    actions.move_to_element(screen).perform()
     table = webdriver.find_element_by_xpath("//div[@id='dataTable']//table[@id='table1']")
     results = []
 
@@ -77,30 +91,40 @@ def collect_data_on_graph(webdriver):
         action.key_down(Keys.ARROW_RIGHT).key_up(Keys.ARROW_RIGHT).perform()
         get_single_data(table, results, screen)
 
-    logger.info(results)
+    logger.info(f"{len(results)} data points collected")
     return results
 
 
-def write_results_to_csv(results, path):
-    logger.info(f"Writing collected data to csv: {path}")
-    with open(path, 'w') as f:
+def write_results_to_csv(results, directory, page, date):
+    if directory == "":
+        filename = f"cn_yield_data {date}.csv"
+    else:
+        filename = f"{directory}\\cn_yield_data {date}.csv"
+
+    logger.info(f"Writing collected data to csv: {filename}")
+    with open(filename, 'w') as f:
+        f.write(f"Source,{page}\n")
+        f.write(f"As of {date}\n\n")
         f.write("Maturity,Yield\n")
         for data in results:
             f.write(f"{data[0]},{data[1]}\n")
 
 
-def main(date):
+def main(date: str, driver_path: str):
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
     formatter = logging.Formatter("%(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    logger.info("Starting firefox...")
-    binary = FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
-    driver = webdriver.Firefox(executable_path=r'C:\cmdtools\geckodriver-v0.28.0-win32\geckodriver.exe',
-                               firefox_binary=binary)
+    logger.info("Starting browser...")
+    # Config options for Chrome
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
 
+    # Init Chrome Driver and negivate to target page
+    driver = Chrome(executable_path=driver_path, options=options)
     page = "http://yield.chinabond.com.cn/cbweb-mn/yield_main?locale=en_US"
     logger.info(f"Loading page {page}")
     driver.get(page)
@@ -138,10 +162,10 @@ def main(date):
 
     driver.close()
 
-    write_results_to_csv(results, "cn_yield_data.csv")
+    write_results_to_csv(results, "", page, date)
 
 
 if __name__ == '__main__':
-    date = "2020-11-30"
-    main(date)
-
+    date = "2020-12-31"  # yyyy-mm-dd
+    driver_path = ""  # path to your Chrome web driver
+    main(date, driver_path)
